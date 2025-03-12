4f93b4242da6b216b00ebcf505f00d067ce99104
Arosan Suresh Balasingam <asb@solita.dk>
2025-03-12 10:45:42 +0100

    Added files.

---
+++ Domain/Interfaces/ITaskManagementReposistory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface ITaskManagementReposistory
+    {
+        Task<IEnumerable<TaskItemEntity>> GetAllAsync();
+        Task<TaskItemEntity> GetByIdAsync(Guid id);
+        Task AddAsync(TaskItemEntity task);
+        Task UpdateAsync(TaskItemEntity task);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ Infrastructure/Persistance/BusinessLogicDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using System.Data.Entity;
+namespace Infrastructure.Persistance
+{
+    public class BusinessLogicDbContext : DbContext
+    {
+        public BusinessLogicDbContext(DbContextOptions<BusinessLogicDbContext> options) : base(options)
+        {
+        }
+        public DbSet<TaskItemEntity> Tasks { get; set; }
+    }
+}
