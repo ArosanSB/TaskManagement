05a0d0c53a9c7b7f22f6062f1717e192136d8198
Arosan Suresh Balasingam <asb@solita.dk>
2025-03-14 10:31:43 +0100

    Added routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { UpdateTaskComponent } from './update-task/update-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+        title: 'Home'
+      },
+      {
+        path: 'tasks',
+        component: TasksComponent,
+        title: 'Tasks'
+      },
+      {
+        path: 'create-task',
+        component: CreateTaskComponent,
+        title: 'Create Task'
+      },
+      {
+        path: 'update-task/:id',
+        component: UpdateTaskComponent,
+        title: 'Update Task'
+      },
+];
