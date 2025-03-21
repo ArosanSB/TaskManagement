694ac583b86307bcf67aef046d8d34ab7e230166
Arosan Suresh Balasingam <asb@solita.dk>
2025-03-21 12:33:03 +0100

    Created test environment for Integration tests.

---
+++ Tests/IntegrationTest/Utility/TestEnvironmnet.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Persistance;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tests.IntegrationTest.Utility;
+
+public class TestEnvironmnet
+{
+    private readonly TestServer _testServer;
+
+    public TestServer TestServer => _testServer;
+
+    public HttpClient HttpClient => _testServer.CreateClient();
+
+    public IServiceScope CreateScope() => _testServer.Services.CreateScope();
+
+    public TestEnvironmnet()
+    {
+        string applicationPath = ".";
+        _testServer = new TestServer(new WebHostBuilder()
+            .UseEnvironment("Testing")
+            .UseContentRoot(applicationPath)
+            .UseConfiguration(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build())
+            .UseStartup<TestEnvironmentStartup>());
+    }
+
+
+    public void CleanDB()
+    {
+        // Clean the database
+        BusinessLogicDbContext context = _testServer.Services.GetRequiredService<BusinessLogicDbContext>();
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+}
